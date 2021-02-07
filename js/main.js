d4eb5732dd763adfe71b6d3a9deea4d5b699e088
Squire <esq.berrouz@gmail.com>
2021-02-07 14:07:24 +0500

    доработал функцию

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ let getRandomNumber = (min, max, fracNumber) => {
 }
 
 getRandomNumber(1.98, 2.01, 2);
+
+
+let getImageRandom = () => {
+  let imageRandom = getRandomNumber(1, 8, 0)
+  return (imageRandom < 10) ? ('0' + imageRandom) : imageRandom;
+}
+
+getImageRandom();
