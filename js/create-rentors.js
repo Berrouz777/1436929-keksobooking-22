ffe442d68928cbf19c0d9807fb7c4161f329a644
Squire <esq.berrouz@gmail.com>
2021-02-22 13:31:23 +0500

    Сделал рефакторинг

--- js/create-rentors.js
+++ js/create-rentors.js
@@ -1,26 +1,16 @@
 import { getRandomNumber } from './util.js  ';
 
-const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const TYPES = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+};
 const TITLES = ['Лучшие отзывы', 'Лучшее обслуживание!', 'ХИТ', 'Лучшее соотношение цена/качество', 'Рекомендуем'];
 const CHECKS = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const RENTORS = 10;
-
-const getRoom = (room) => {
-  switch (room) {
-    case 'palace':
-      return 'Дворец';
-    case 'flat':
-      return 'Квартира';
-    case 'house':
-      return 'Дом';
-    case 'bungalow':
-      return 'Бунгало';
-    default:
-      return 'На улице :)';
-  }
-}
 
 const getImageRandom = () => {
   let imageRandom = getRandomNumber(1, 8, 0)
@@ -51,7 +41,7 @@ const createRentor = () => {
       title: getRandomElementArray(TITLES),
       address: '{{location.x}}, {{location.y}}',
       price: getRandomNumber(1000, 10000, 0),
-      type: getRoom(getRandomElementArray(TYPES)),
+      type: getRandomElementArray(Object.values(TYPES)),
       rooms: getRandomNumber(2, 4, 0),
       guests: getRandomNumber(1, 10, 0),
       checkin: getRandomElementArray(CHECKS),
@@ -68,6 +58,6 @@ const createRentor = () => {
 }
 
 const createRentors = new Array(RENTORS).fill(null).map(() => createRentor());
-createRentors();
+createRentors;
 
 export { createRentor, RENTORS };
